745ea129893fb272e7beb45b6dd9ef911ef20ac4
innysik <innysik@hotmail.com>
2022-05-03 12:55:27 +0200

    simplielst linked list

---
+++ Custom_MyLinkedList/src/com/company/linkedlist/MyList.java
@@ -0,0 +1,13 @@
+package com.company.linkedlist;
+
+public class MyList {
+    public static void main(String[] args) {
+        MyLinkedListAnotherOne poezd = new MyLinkedListAnotherOne();
+        poezd.addToHead(7);
+        poezd.addToHead(9);
+        poezd.addToHead(12);
+        poezd.print();
+        poezd.addToTail(3);
+        poezd.print();
+    }
+}
