305aa889825a8bffc9741afdcafe236accb51d07
ella-morash <69237909+ella-morash@users.noreply.github.com>
2022-06-24 12:55:39 +0200

    homework 24 06

--- 17_06_practice/src/com/company/Main.java
+++ 17_06_practice/src/com/company/Main.java
@@ -2,6 +2,8 @@ package com.company;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class Main {
 
@@ -37,6 +39,31 @@ public class Main {
 
         return res;
 
+    }
+
+    //написать метод, принимающий строку, содержащую слова разделенные пробелом и два
+// функциональных интерфейса  function and predicate. Метод должен предоставлять воз
+//можность реализации следующих действий:
+// при длине слова 3 изменить все буквы этого слова на строчные и вернуть измененную строку
+// при длине слова 3 изменить все буквы этого слова на заглавные и вернуть измененную строку
+// при длине слова 4 изменить все буквы этого слова на строчные и вернуть измененную строку
+// при длине слова 4 изменить все буквы этого слова на заглавные и вернуть измененную строку
+// при длине слова 5 изменить все буквы этого слова на звездочки и вернуть измененную строку
+    //       (т.е.написать лямбду для каждого случая )
+
+    public static String transform(String input, Predicate<String> predicate, Function<String,String> function) {
+        String[] stringArr = input.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (String word:stringArr) {
+            if (predicate.test(word)) {
+                sb.append(function.apply(word)).append(" ");
+
+            } else {
+                sb.append(word).append(" ");
+            }
+
+        }
+        return sb.toString().trim();
     }
 
 
